ff8443fbfe832c10bb895fbe0ee9aed0d0abbc18
JaennPB <jaenn.bucio@gmaill.com>
2022-07-19 15:14:08 -0500

    adding box slider animations

--- components/BoxItem.tsx
+++ components/BoxItem.tsx
@@ -1,26 +1,54 @@
-import { Dimensions } from "react-native";
-import { View, Text } from "native-base";
+import { Dimensions, StyleSheet } from "react-native";
+import { Text } from "native-base";
+
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
 
 interface Props {
   title: string;
   index: number;
+  translateX: Animated.SharedValue<number>;
 }
 
 const { width } = Dimensions.get("window");
 
-const BoxItem: React.FC<Props> = ({ title, index }) => {
+const BoxItem: React.FC<Props> = ({ title, index, translateX }) => {
+  const rStyle = useAnimatedStyle(() => {
+    const translateY = interpolate(
+      translateX.value,
+      [
+        (index - 1) * (width * 0.8),
+        index * (width * 0.8),
+        (index + 1) * (width * 0.8),
+      ],
+      [0, -30, 0]
+    );
+
+    return {
+      transform: [{ translateY: translateY }],
+    };
+  });
+
   return (
-    <View
-      key={index}
-      w={width * 0.7}
-      bg="red.100"
-      borderRadius={30}
-      justifyContent="center"
-      alignItems="center"
-    >
+    <Animated.View key={index} style={[styles.container, rStyle]}>
       <Text>{title}</Text>
-    </View>
+    </Animated.View>
   );
 };
 
 export default BoxItem;
+
+const styles = StyleSheet.create({
+  container: {
+    width: width * 0.7,
+    height: width * 0.7,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+    backgroundColor: "#cf8d8d",
+    marginLeft: width * 0.1,
+  },
+});
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,34 +1,48 @@
 import { Dimensions, ListRenderItemInfo } from "react-native";
-import { Flex, Box, FlatList, View } from "native-base";
-
-import Animated from "react-native-reanimated";
+import { Flex, FlatList, View, ScrollView } from "native-base";
 
 import BoxItem from "../components/BoxItem";
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
 
-const { height, width } = Dimensions.get("window");
+const { height } = Dimensions.get("window");
 
-const BOXES = ["Box 1", "Box 2", "Box 3", "Box 4"];
+const BOXES = ["Box 1", "Box 2", "Box 3", "Box 4", "Box 5"];
 
 const HomeScreen: React.FC = () => {
-  function renderBoxItemHandler(itemData: ListRenderItemInfo<string>) {
-    return <BoxItem title={itemData.item} index={itemData.index} />;
-  }
+  const XScrollData = useSharedValue(0);
+
+  const xScrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      XScrollData.value = event.contentOffset.x;
+    },
+  });
 
   return (
-    <Flex flex={1} justify="center">
-      <Box h={height / 2} bg="amber.100">
-        <FlatList
-          data={BOXES}
-          renderItem={renderBoxItemHandler}
+    <Flex flex={1} justify="center" bg="amber.200">
+      <Flex h={height / 2} bg="white" justify="center" align="center">
+        <Animated.ScrollView
           horizontal
           showsHorizontalScrollIndicator={false}
-          decelerationRate={0}
-          //   snapToInterval={width * 0.8}
-          //   snapToAlignment="center"
+          decelerationRate={0.5}
           scrollEventThrottle={16}
-          ItemSeparatorComponent={() => <View w={width * 0.1} />}
-        />
-      </Box>
+          onScroll={xScrollHandler}
+          contentContainerStyle={{
+            alignItems: "center",
+          }}
+        >
+          {BOXES.map((box, index) => (
+            <BoxItem
+              key={index}
+              title={box}
+              index={index}
+              translateX={XScrollData}
+            />
+          ))}
+        </Animated.ScrollView>
+      </Flex>
     </Flex>
   );
 };
